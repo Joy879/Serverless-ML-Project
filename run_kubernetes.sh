5f3f61028b495497fcd810990d98a707a381708c
Joy879 <joywanjiru879@gmail.com>
2023-02-28 19:27:34 +0000

    Update necessary files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=joywanjiru/serverless
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run serverless\
+	--image=$dockerpath\
+	--port=80 --labels app=serverless
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward serverless 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=joywanjiru/serverless
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker --tag serverless $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
